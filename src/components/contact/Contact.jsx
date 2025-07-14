3f6ea166cfe5955aa4f99deae3f0e97358c66b0a
stwhello <sherinpaul2001@gmail.com>
2025-07-14 17:38:08 +0530

    day six

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,29 @@
-import "./contact.css"
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div className='contact'>Contact</div>
-  )
-}
+    <div className='contact'>
+      <div className='cSection'>
+        <form>
+          <h1 className='cTitle'>Let's Keep in Touch</h1>
+          <div className='formItem'>
+            <label>Name</label>
+            <input type='text' placeholder='John Doe' />
+          </div>
+          <div className='formItem'>
+            <label>Email</label>
+            <input type='email' placeholder='johndoe@gmail.com' />
+          </div>
+          <div className='formItem'>
+            <label>Message</label>
+            <textarea rows={10} placeholder='Write your message..'></textarea>
+          </div>
+          <button className='formButton'>Send</button>
+        </form>
+      </div>
+      <div className='cSection'>SVG</div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
