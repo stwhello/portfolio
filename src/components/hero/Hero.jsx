ee875e4317ec580db9b4fd128a0fa6dd125f7e99
stwhello <sherinpaul2001@gmail.com>
2025-08-11 16:59:17 +0530

    contact section done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -126,7 +126,7 @@ const Hero = () => {
           </motion.a>
           <motion.a
             variants={followVariants}
-            href='https://drive.google.com/file/d/1B2Wai8_jb0-CgZDMSbEitRSdwzZ_GoPP/view?usp=sharing'
+            href='https://drive.google.com/file/d/1AR_tpESTEDY5Jn91l3K-22YLGEaPDJBD/view?usp=sharing'
             target='_blank'
             rel='noopener noreferrer'
           >
